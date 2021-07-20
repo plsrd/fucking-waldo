331cdc3d9e0bcd1ed15496bf5636f6ea67d1f251
racheallarimer <racheal.larimer@gmail.com>
2021-07-20 11:40:33 -0700

    add level number and preview to level schema

--- studio/schemas/level.js
+++ studio/schemas/level.js
@@ -4,6 +4,11 @@ export default {
   type: 'document',
   fields: [
     {
+      name: 'number',
+      title: 'Level Number',
+      type: 'number'
+    },
+    {
       name: 'mainImage',
       title: 'Main Image',
       type: 'image'
@@ -16,5 +21,15 @@ export default {
         {type: 'character'}
       ]
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'number'
+    },
+    prepare({ title }) {
+      return {
+        title: `Level ${title}`
+      }
+    }
+  }
 }
